27d3c236d838c8f01597f99ed25c1e466957ebd8
Keflem trindade <keflemtn@gmail.com>
2020-08-03 20:08:04 -0400

    ajustando bottombar

--- lib/home.dart
+++ lib/home.dart
@@ -6,35 +6,66 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      //define cores padroes pros icons
-      iconTheme: IconThemeData(color: Colors.grey, opacity: 1),
-      backgroundColor: Colors.white,
-      title: Image.asset(
-        "img/youtube.png",
-        width: 98,
-        height: 22,
+      appBar: AppBar(
+        //define cores padroes pros icons
+        iconTheme: IconThemeData(color: Colors.grey, opacity: 1),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          "img/youtube.png",
+          width: 98,
+          height: 22,
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                print("Acao conta!");
+              }),
+          IconButton(
+              icon: Icon(Icons.videocam),
+              onPressed: () {
+                print("Acao busca");
+              }),
+          IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {
+                print("Acao videocam!");
+              })
+        ],
       ),
-      actions: <Widget>[
-        IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print("Acao conta!");
-            }),
-        IconButton(
-            icon: Icon(Icons.videocam),
-            onPressed: () {
-              print("Acao busca");
-            }),
-        IconButton(
-            icon: Icon(Icons.account_circle),
-            onPressed: () {
-              print("Acao videocam!");
-            })
-      ],
-    ));
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _indiceAtual,
+          onTap: (indice) {
+            setState(() {
+              _indiceAtual = indice;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.red,
+          items: [
+            BottomNavigationBarItem(
+                backgroundColor: Colors.red,
+                title: Text("Inicio"),
+                icon: Icon(Icons.home)),
+            BottomNavigationBarItem(
+                backgroundColor: Colors.amber,
+                title: Text("Em alta"),
+                icon: Icon(Icons.whatshot)),
+            BottomNavigationBarItem(
+                backgroundColor: Colors.green,
+                title: Text("Inscrições"),
+                icon: Icon(Icons.subscriptions)),
+            BottomNavigationBarItem(
+                backgroundColor: Colors.blue,
+                title: Text("Bilioteca"),
+                icon: Icon(Icons.folder))
+          ]),
+    );
   }
 }
